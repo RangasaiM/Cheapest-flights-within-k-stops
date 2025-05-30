c502e8f1d51f9d6dbb627ad47aabb887b0132539
Rangasai <rangasaimangalagiri@gmail.com>
2025-05-30 15:09:19 +0530

    test java

--- solutions/Solution.java
+++ solutions/Solution.java
@@ -9,5 +9,6 @@ import java.util.Queue;
 public class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         // Write your code here
+        return -1;
     }
 }
